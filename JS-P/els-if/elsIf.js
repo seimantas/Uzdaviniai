e26fd7dd5ca40ac9ba4576048bc8979a27e548e1
Eimantas <eimantas@Eimantass-MacBook-Air.local>
2022-09-07 22:17:02 +0300

    upload

---
+++ JS-P/els-if/elsIf.js
@@ -0,0 +1,24 @@
+const legalAge = (20);
+
+const clientAge = (17);
+
+if (clientAge < 20) {
+    alert("ilegalAge");
+} else {
+    alert("legalAge");
+}
+
+const firstName = "eimantas"
+
+if (firstName.length > 6) {
+    alert("ilgasVardas");
+}
+
+const myAge = (32)
+
+if (myAge > 100 || myAge < 0) {
+    alert("Invalid age");
+} else if (myAge > 19 || myAge < 99) {
+    alert("Adult");
+} else (myAge > 1 || myAge < 18)
+alert("Child");
