48ade2e213170553a484733de5b9dd195d5e9cf6
Eimantas <eimantas@Eimantass-MacBook-Air.local>
2022-09-08 21:39:52 +0300

    uload

---
+++ JS-P/els-if/cars.js
@@ -0,0 +1,11 @@
+const car = prompt("type car make");
+
+if (car === 'VW' || car === 'Audi' || car === 'Bentley.' || car === 'Bugatti' || car === 'Lamborghini' || car === 'Porsche') {
+    console.log('VW Group');
+
+} else if (car === 'BMW' || car === 'Mini' || car === 'Rolls-Royce') {
+    alert('BMW Group');
+
+} else {
+    alert('Nei vienam');
+}
