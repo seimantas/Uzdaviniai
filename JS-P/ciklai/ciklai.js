036351b49057135457db921b81196e2f074028d8
Eimantas <eimantas@Eimantass-MacBook-Air.local>
2022-09-13 21:30:27 +0300

    Upload

---
+++ JS-P/ciklai/ciklai.js
@@ -0,0 +1,18 @@
+const firstName = ("Eimantas");
+const count = 10;
+
+for (let firstName = 0; firstName < 10; firstName++) {
+    console.log("Eimantas");
+}
+
+for (let firstName = 0; firstName < count; firstName++) {
+    console.log(firstName);
+}
+
+for (let firstName = 0; firstName < 10; firstName++) {
+    console.log(firstName, ("Eimantas"));
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
