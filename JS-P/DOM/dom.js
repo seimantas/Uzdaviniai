f11ee0a66d0fa9a0c47c1c98e3a88f6605988b82
Eimantas <eimantas@Eimantass-MacBook-Air.local>
2022-09-19 22:22:55 +0300

    Upload

---
+++ JS-P/DOM/dom.js
@@ -0,0 +1,8 @@
+const firstName = "Eimantas";
+
+const h2 = document.createElement("h2");
+
+h2.textContent = firstName;
+document.body.append(h2);
+
+document.getElementById("maneName").textContent = firstName;
