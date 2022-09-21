34e9610391e76f19a7a2c044ae4c4e19f9af3526
Eimantas <eimantas@Eimantass-MacBook-Air.local>
2022-09-21 21:00:12 +0300

    Upload

--- JS-P/DOM/dom.js
+++ JS-P/DOM/dom.js
@@ -2,7 +2,22 @@ const firstName = "Eimantas";
 
 const h2 = document.createElement("h2");
 
+const firstListItem = document.querySelector("ol li:first-child");
+
+const secondListItem = document.querySelector("ol li:nth-child(2)");
+
+const bmw_group = firstListItem.textContent;
+
+const vw_group = secondListItem.textContent;
+
 h2.textContent = firstName;
 document.body.append(h2);
 
 document.getElementById("maneName").textContent = firstName;
+
+document.querySelector("ul li:last-child").textContent = "Sūris";
+
+document.querySelector(".bluetext > span").textContent = "blue";
+
+firstListItem.textContent = vw_group;
+secondListItem.textContent = bmw_group;
