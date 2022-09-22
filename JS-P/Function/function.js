0821180fb7627b9d3afdd63f2381f3bdbc3e95f4
Eimantas <eimantas@Eimantass-MacBook-Air.local>
2022-09-22 21:23:42 +0300

    Refresh

--- JS-P/Function/function.js
+++ JS-P/Function/function.js
@@ -14,3 +14,61 @@ const getFirstNameAndSurnameLength = (firstName, surname) => {
 };
 
 console.log(getFirstNameAndSurnameLength("Eimantas", "sipas"));
+
+const getLetterByIndex = (i) => {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return alphabet[i];
+};
+
+console.log(getLetterByIndex(6));
+
+const calculate = (n1, n2, operator) => {
+  switch (operator) {
+    case "sum":
+      return n1 + n2;
+    case "sub":
+      return n1 - n2;
+    case "div":
+      return n1 / n2;
+    case "multi":
+      return n1 / n2;
+  }
+};
+console.log(calculate(1, 2, "sum"));
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 10 + 1);
+};
+
+const squareNumber = (generateRandomNumber) => {
+  return Math.pow(generateRandomNumber, 2);
+};
+
+console.log(squareNumber(generateRandomNumber()));
